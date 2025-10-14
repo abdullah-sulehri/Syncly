ecda9fb9cea64ddad81b3a1d0d406a7e933f18a1
Abdullah Sulehri <abdullah.sulehri.11@gmail.com>
2025-10-14 23:53:29 +0500

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { ENV } from "./config/env.js";
+
+
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello world1233")
+})
+
+console.log ("MONGO_URI : ",ENV.MONGO_URI)
+
+app.listen(ENV.PORT,()=>{
+    console.log("Server started on port : ",ENV.PORT)
+})
